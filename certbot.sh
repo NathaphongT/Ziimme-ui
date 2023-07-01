b12b9c12df3ae8c8a8ac9f3d0ef62a5102c05ca2
Your Name <you@example.com>
2023-07-02 01:15:17 +0700

    [MOD] certbot for set dns cloudflare

--- certbot.sh
+++ certbot.sh
@@ -6,7 +6,6 @@ CONTAINER_NAME="certbot"
 DIR_ETC="/server/Zimme-ui/letsencrypt"
 DIR_LIB="/server/Zimme-ui/lib/letsencrypt"
 DIR_CLOUD="/server/Ziimme-ui/cloudflare"
-DIR_LOG="/server/Ziimme-ui/logl"
 
 # ------------------------------------------------------------------------------------------
 sudo rm -rf $DIR_ETC
@@ -17,7 +16,6 @@ sudo docker run -it --rm --name $CONTAINER_NAME \
                 -v $DIR_ETC:/etc/letsencrypt \
                 -v $DIR_LIB:/var/lib/letsencrypt \
                 -v $DIR_CLOUD:/cloudflare \
-                -v $DIR_LOG:/var/log/letsencrypt/letsencrypt.log \
                 $IMAGE_NAME certonly \
                 --dns-cloudflare \
                 --dns-cloudflare-credentials /cloudflare/cloudflare.ini \
