99b9da08a12ee98cdc27bb165c236e2cd2994a47
Your Name <you@example.com>
2023-07-07 21:20:10 +0700

    [MOD] update prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   appVersion: require('../../package.json').version,
   production: true,
   // APIURL_LOCAL: "http://localhost:8080",
-  APIURL_LOCAL: "http://wiimme-service:8080",
+  APIURL_LOCAL: "https://ziimmegroup.com/backend",
 };
