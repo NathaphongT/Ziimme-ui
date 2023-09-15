c74e2560a02016a5dd6ebf3250624fee30f197c2
Your Name <you@example.com>
2023-09-16 00:24:32 +0700

    [MOD] update script for renew cert on cloudflare

--- renew.sh
+++ renew.sh
@@ -2,11 +2,12 @@
 #!/bin/bash
 
 # ------------------------------------ Certbot Renew -----------------------------------------
-IMAGE_NAME=certbot/certbot
+IMAGE_NAME=certbot/dns-cloudflare
 CONTAINER_NAME="certbot"
 DIR_ETC="/server/Ziimme-ui/letsencrypt"
 DIR_LIB="/server/Ziimme-ui/lib/letsencrypt"
 DIR_LOG="/server/Ziimme-ui/logs"
+DIR_CLOUD="/server/Ziimme-ui/cloudflare"
 
 # ---------------------------------------------------------------------------------------------
 sudo docker stop wiimme
@@ -14,5 +15,6 @@ sudo docker run -it --rm --name $CONTAINER_NAME \
                 -v $DIR_ETC:/etc/letsencrypt \
                 -v $DIR_LIB:/var/lib/letsencrypt \
                 -v $DIR_LOG:/var/log/letsencrypt \
+                -v $DIR_CLOUD:/cloudflare \
                 $IMAGE_NAME renew
 sudo docker start wiimme
